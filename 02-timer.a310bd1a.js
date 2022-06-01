6fbc399f03bba5b45b935d9e9531989658c4b3dd
AleksandrPav <94559297+AleksandrPav@users.noreply.github.com>
2022-06-01 09:44:46 +0000

    Deploying to gh-pages from @ AleksandrPav/goit-js-hw-09@c59615d08899fdfb022ef346d27618c886f5fc2d 🚀

--- 02-timer.a310bd1a.js
+++
@@ -1,2 +0,0 @@
-var refs={input:document.querySelector("#datetime-picker")};console.log(refs),flatpickr(refs.input,{options:options});var options={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose:function(e){console.log(e[0])}};
-//# sourceMappingURL=02-timer.a310bd1a.js.map
