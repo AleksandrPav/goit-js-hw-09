c59615d08899fdfb022ef346d27618c886f5fc2d
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-06-01 12:42:09 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,19 @@
 
+import flatpickr from "flatpickr";
+import "flatpickr/dist/themes/material_red.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 
 const refs = {
-    input: document.querySelector("#datetime-picker"),
+  input: document.querySelector("#datetime-picker"),
+  btnStart: document.querySelector("button[data-start]"),
+  days: document.querySelector("span[data-days]"),
+  hours: document.querySelector("span[data-hours]"),
+  minutes: document.querySelector("span[data-minutes]"),
+  seconds: document.querySelector("span[data-seconds]"),
 }
 console.log(refs);
-
-
-flatpickr(refs.input, {options});
-
-
 
 const options = {
   enableTime: true,
@@ -20,3 +24,26 @@ const options = {
     console.log(selectedDates[0]);
   },
 };
+
+
+flatpickr(refs.input, {options});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
